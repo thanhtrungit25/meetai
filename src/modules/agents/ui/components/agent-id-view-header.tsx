583c5c3c47ac0bf88726f5e8f12ff8e728b19913
Trung DT <thanhtrung.it25@gmail.com>
2025-05-30 13:23:16 +0700

    Agent page

---
+++ src/modules/agents/ui/components/agent-id-view-header.tsx
@@ -0,0 +1,79 @@
+'use client';
+
+import Link from "next/link";
+import { ChevronRightIcon, TrashIcon, PencilIcon, MoreVerticalIcon } from "lucide-react";
+
+import {
+  Breadcrumb,
+  BreadcrumbItem,
+  BreadcrumbLink,
+  BreadcrumbList,
+  BreadcrumbSeparator,
+} from "@/components/ui/breadcrumb"
+
+import {
+  DropdownMenu,
+  DropdownMenuTrigger,
+  DropdownMenuItem,
+  DropdownMenuContent,
+} from "@/components/ui/dropdown-menu";
+
+import { Button } from "@/components/ui/button";
+
+type Props = {
+  agentId: string;
+  agentName: string;
+  onEdit: ()=>void;
+  onRemove: ()=>void;
+}
+
+export default function AgentIdViewHeader({
+  agentId,
+  agentName,
+  onEdit,
+  onRemove,
+}: Props) {
+  return (
+    <div className="flex items-center justify-between">
+      <Breadcrumb>
+        <BreadcrumbList>
+          <BreadcrumbItem>
+            <BreadcrumbLink asChild className="font-medium text-xl">
+              <Link href='/agents'>
+                My Agents
+              </Link>
+            </BreadcrumbLink>
+          </BreadcrumbItem>
+          <BreadcrumbSeparator className="text-foreground text-xl font-medium [&>svg]:size-4">
+            <ChevronRightIcon />
+          </BreadcrumbSeparator>
+          <BreadcrumbItem>
+            <BreadcrumbLink asChild className="font-medium text-xl text-foreground">
+              <Link href={`/agents/${agentId}`}>
+                {agentName}
+              </Link>
+            </BreadcrumbLink>
+          </BreadcrumbItem>
+        </BreadcrumbList>
+      </Breadcrumb>
+
+      <DropdownMenu modal={false}>
+        <DropdownMenuTrigger asChild>
+          <Button variant='ghost'>
+            <MoreVerticalIcon />
+          </Button>
+        </DropdownMenuTrigger>
+        <DropdownMenuContent align="end">
+          <DropdownMenuItem>
+            <PencilIcon className="size-4 text-black" />
+            Edit
+          </DropdownMenuItem>
+          <DropdownMenuItem>
+            <TrashIcon className="size-4 text-black" />
+            Delete
+          </DropdownMenuItem>
+        </DropdownMenuContent>
+      </DropdownMenu>
+    </div>
+  );
+};
